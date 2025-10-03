c31e65fd42710079989bf0c348a47c84f311aafe
Girishkumar yadagiri <yadagirigiri29@gmail.com>
2025-10-03 15:21:01 +0530

    updated Introjs.js

--- Introjs.js
+++ Introjs.js
@@ -45,3 +45,6 @@ function cab(name, type) {
         console.log("Before fetching data");
         fetchData();
         console.log("After fetching data"); 
+        const arr = [1, 2, 3, 4, 5];
+        const squaredArr = arr.map(num => num * num);
+        console.log(squaredArr);    
