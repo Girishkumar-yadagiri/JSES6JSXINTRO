61af0bbbe13268ea0eea07faa52b90c961394dca
Girishkumar yadagiri <yadagirigiri29@gmail.com>
2025-10-03 14:11:55 +0530

    updated introjs.js, method, objects creation,accessor,properties,prototypes

--- Introjs.js
+++ Introjs.js
@@ -1,3 +1,23 @@
 function greetuser(){
             alert("Hello User");
         }
+
+
+function cab(name, type){
+    this.name = name;
+    this.type = type;
+    this.details = function(){
+        return `${this.name} is a +${this.type} cab`;
+    }
+
+}
+const cab1 = new cab("Uber", "Sedan");
+cab.prototype.start = function(){
+    return `${this.name} cab has started`;
+} 
+cab.prototype.stop = function(){
+    return `${this.name} cab has stopped`;
+}
+cab.prototype.move = function(){
+    return `${this.name} cab is moving`;
+}     
